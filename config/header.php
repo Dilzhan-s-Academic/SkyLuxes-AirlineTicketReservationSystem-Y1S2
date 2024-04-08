0f18a1aa8508756d81a55442c5cc54e63f65f2b0
ChamithDilshan <61306553+DilZhaan@users.noreply.github.com>
2024-04-08 18:43:26 +0530

    DilZhan Yapa add some config files and create logo for web

---
+++ config/header.php
@@ -0,0 +1,13 @@
+<!--Dilshan Yapa-->
+<!-- <?php session_start(); ?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <link rel="stylesheet" href="../styles/header.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php include"./config/header.php" ?>
+    </body>
+</html>
