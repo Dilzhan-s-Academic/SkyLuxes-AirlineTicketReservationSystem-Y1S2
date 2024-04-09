6160fa8807d418c69b27328ca2320bf82da142f3
ChamithDilshan <61306553+DilZhaan@users.noreply.github.com>
2024-04-09 23:51:04 +0530

    add general css and create search form on home page it23366572

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <html lang="en">
 
     <head>
+        <link rel="stylesheet" type="text/css" href="styles/generalStyle.css">
         <link rel="stylesheet" type="text/css" href="styles/header.css">
         <link rel="stylesheet" type="text/css" href="styles/index.css">
         
@@ -54,6 +55,50 @@
             <span class="dot"></span> 
         </div>
 
+        <div class= "SearchFlightsForm">
+            <span>Book a Trip</span>
+            <div class="form-div">
+                <form> 
+                    <select name="DepartureAirport" id="DepartureAirport">
+                        <option disabled selected value> FROM </option>
+                        <option value="abc">abc</option>
+                        <option value="ddd">ddd</option>
+                        <option value="333">333</option>
+                    </select>
+                    <input type="date" id="DayToGo">
+                    <input type="date" id="DayToCome">
+                    <br/>
+                    <select name="DestinationAirport" id="DestinationAirport">
+                        <option disabled selected value> TO </option>
+                        <option value="abc">abc</option>
+                        <option value="ddd">ddd</option>
+                        <option value="333">333</option>
+                    </select>
+                    <select name="SheetCount" id="SheetCount">
+                        <option disabled selected value> Sheet Count </option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </select>
+                    <select name="SheetClass" id="SheetClass">
+                        <option disabled selected value> Class </option>
+                        <option value="abc">Economy</option>
+                        <option value="ddd">Business</option>
+                    </select>
+                    <br/>
+                    <input type="radio" id="TripType1" value="OneWay" name="TripType"> Oneway
+                    <input type="radio" id="TripType2" value="Returm" name="TripType"> Return
+                    <input type="Submit" id="Search" value="Search">
+                </form>
+            </div>
+        </div>
         <script src="js/slideshow.js"></script>
     </body>
 </html>
