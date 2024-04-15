ed7b1526ebecdf93e5d92b2f59b043c89d6a36d7
ChamithDilshan <61306553+DilZhaan@users.noreply.github.com>
2024-04-15 14:13:28 +0530

    addsome comments it23366572

--- js/popupWindow.js
+++ js/popupWindow.js
@@ -1,3 +1,6 @@
+// Dilshan Yapa S Y C T it23366572
+
+
 function openPopup(elementID) {
     var viewReservation = document.getElementById(elementID);
     viewReservation.style.visibility = "visible";
